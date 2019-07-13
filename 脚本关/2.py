d904a3399178d86fece8bb0b2b758d874571823c
Firmg1 <51110346+Firmg1@users.noreply.github.com>
2019-07-13 20:30:18 +0800

    Update 2.py

--- 脚本关/2.py
+++ 脚本关/2.py
@@ -1,11 +1,5 @@
 import requests
 import re
-from bs4 import BeautifulSoup
-
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
-    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
-}
 url = 'http://lab1.xseclab.com/xss2_0d557e6d2a4ac08b749b61473a075be1/index.php'
 s=requests.session()
 request = s.get(url)
@@ -17,6 +11,5 @@ ree = r1 * r2 + r3 * (r1 + r2)
 data = {
     'v': str(ree)
 }
-res = s.post(url, data=data)
-soup = BeautifulSoup(res.content, 'lxml')
-print(soup)
+res = s.post(url, data=data).content
+print(res.decode('utf-8'))
